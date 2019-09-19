879a0b483ec5fa281cc3b5edbe34cb1af7cb6c0b
sam <sostep99@harrisonburg.k12.va.us>
2019-09-19 09:45:02 -0400

    Update game.js

--- game.js
+++ game.js
@@ -4,10 +4,18 @@ var message2 = ("you got to 100")
 postToPage(score); // Print to the page
 
 setInterval(btnPress, 1000);   // Set up a loop
+let button3 = new Button('hi', btnPress); // Make a button
+button3.hide()
 
+
+createButton('Button', btnPress);
 function btnPress() {
   score = score + 2
   postToPage(score);
+  if(score > 10) {
+    button3.show();    
+             }           // If the score reaches 11, show the button!
+
 }
 
 createButton('Button2', btnPress2);
@@ -22,6 +30,11 @@ function btnPress2() {
 if(score < 100){
   createText(message);
 }
-if(score > 100){
-  createText(message2);
+
+if(score > 10) {
+  button3.show();               // If the score reaches 11, show the button!
 }
+
+
+
+
