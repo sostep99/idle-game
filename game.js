382e552538064332982e1a90f9107e92487c3674
sam <sostep99@harrisonburg.k12.va.us>
2019-09-23 09:54:18 -0400

    Update game.js
    gdfgs

--- game.js
+++ game.js
@@ -1,35 +1,27 @@
-var score = 1; // Create a variable to store the score in
+var score = 100; // Create a variable to store the score in
 
 var title = new Title('Welcome to the Game'); // Create a title element
-var button = new Button('hello', btnPress);   // Create a button element
-var hidden = new Button('Boo!');
-hidden.hide();
+var button = new Button('Soul', btnPress);         // Create a button element
 var scr = new Text(score);                    // Create a text element
+var button2 = new Button('enemy', btnPress2); // Make a button
+button2.hide()
+setInterval(btnPress2, 115); 
 
-setInterval(btnPress, 1000);   // Set up a loop
-let button3 = new Button('hi', btnPress); // Make a button
-button3.hide()
+   
 
-
-createButton('Button', btnPress);
 function btnPress() {
   score++;         // Increase the score
   scr.edit(score); // Update the page with the new score
-  if(score > 10) {
-    hidden.show();
-  }
-  if(score > 15) {
-    hidden.hide(500);
-  }
 }
-if(score < 100){
-  createText(message);
+let text1   = new Text('You wake up in a dark room');
+let text2   = new Text('In front of you is a button labled Souls, and next to it is a Soul counter it says you curently have 100000 souls');
+let text3   = new Text('Beyond the button is a dark tunnel');
+let text4   = new Text('Suddenly from the darkness auge red scorpion suddenly jumps out and starts attacking the Soul counter');
+
+function btnPress2() {
+  score--;         // Increase the score
+  scr.edit(score); // Update the page with the new score
 }
-
-if(score > 10) {
-  button3.show();               // If the score reaches 11, show the button!
+if(score <= 0) {
+  
 }
-
-
-
-
