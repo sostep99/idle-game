38d4a86e7e3089df9bb7c27c48b47fb5aef51557
sam <sostep99@harrisonburg.k12.va.us>
2019-09-24 09:52:08 -0400

    Update game.js
    added timer and scorpion

--- game.js
+++ game.js
@@ -3,14 +3,22 @@ var timer = 20;
 var title = new Title('Welcome to the Game'); // Create a title element
 var button = new Button('Soul', btnPress);         // Create a button element
 var scr = new Text(score);                    // Create a text element
+var tim = new Text(timer)
 var button2 = new Button('enemy', btnPress2); // Make a button
 var button3 = new Button('timer', btnPress3);
-var tim = new Text(timer)
+var text1   = new Text('You wake up in a dark room ' + 'In front of you is a button labled Souls, and next to it is a Soul counter it says you curently have 100 souls ' + 'Beyond the button is a dark tunnel ' + 'above the tunnel is timer slowly counting towards 0'); 
+var text2   = new Text('Suddenly from the darkness auge red scorpion suddenly jumps out and starts attacking the Soul counter ' + 'YOU LOSE -1 SOULS PER SECOND');
+var next = false
 button2.hide()
 button3.hide()
+text2.hide()
 
 
-setInterval(btnPress3, 1000); 
+
+
+
+
+setInterval(btnPress3, 400); 
 
 
 
@@ -24,11 +32,17 @@ function btnPress() {
   score++;         // Increase the score
   scr.edit(score); // Update the page with the new score
 }
-let text1   = new Text('You wake up in a dark room');
-let text2   = new Text('In front of you is a button labled Souls, and next to it is a Soul counter it says you curently have 100000 souls');
-let text3   = new Text('Beyond the button is a dark tunnel');
-let text4   = new Text('above the tunnel is timer slowly counting towards 0');
 
+
+var rowSection = new Section('row'); // Create the row to store the columns in
+
+var column1 = new Section('col');    // Create the first column
+var column2 = new Section('col');    // Create the second column
+
+rowSection.add(column1);  // Add the first column to the row
+rowSection.add(column2);  // Add the second column to the row
+
+column1.add(text1);  // Add the text to the first column
 
 
 
@@ -38,9 +52,12 @@ function btnPress2() {
 }
 
 function scorpion() {
-  if(timer == 0) {
+  if(timer == 0 && next == false) {
 setInterval(btnPress2, 1000); 
-let text5   = new Text('Suddenly from the darkness auge red scorpion suddenly jumps out and starts attacking the Soul counter');
-let text6   = new Text('YOU LOSE -1 SOULS PER SECOND');
+column2.add(text2);
+text2.show()
+timer = timer + 30
+next = true
   }
+
 }
